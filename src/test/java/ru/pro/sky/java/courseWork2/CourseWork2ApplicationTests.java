385734b874bf86704f471dccdda8367b288f08c2
ad_hoc <ad_hoc90@mail.ru>
2023-08-30 22:08:36 +0300

    initial commit

---
+++ src/test/java/ru/pro/sky/java/courseWork2/CourseWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pro.sky.java.courseWork2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
